6087e08ec8bf5273ad2a6a443303290f117b36e6
Siddharth Mukherjee <68189505+HappyBoySidd@users.noreply.github.com>
2024-11-21 23:52:23 +0530

    Added new helper

--- src/helpers/product.js
+++ src/helpers/product.js
@@ -3,8 +3,13 @@ import Products from "../models/product.js";
 export const getProducts = async () =>
   await Products.find({}).lean();
 
-export const getProductById = async (productId) =>
-    await Products.findOne({ productId }).lean();
+export const getProductById = async (productId) => {
+  const details = await Products.findOne({
+    _id: productId,
+  }).lean();
+
+  return details;
+};
 
 export const isProductPresent = async (productId) =>
   await getProductById(productId) ? true : false;
