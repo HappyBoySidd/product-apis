4a10dd0b93292afee59807e5850cf60ace5aea7f
Siddharth Mukherjee <68189505+HappyBoySidd@users.noreply.github.com>
2024-11-22 00:01:58 +0530

    added new api - getProductssByIds

--- src/routes/product.js
+++ src/routes/product.js
@@ -8,5 +8,6 @@ router.get("/:productId", productController.getProduct);
 router.post("/addProduct", productController.addproduct);
 router.put("/:productId", productController.updateProduct);
 router.delete("/:productId", productController.deleteProduct);
+router.get("/listProducts/:productIds", productController.getProductssByIds);
 
 export default router;
